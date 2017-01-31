f8e008cdb86e2880a8b197b96b6e4ec4ab2ce1e7
Yun Kui Pan <panyunkui@gmail.com>
2017-01-31 10:33:18 -0500

    Did functions to alert posible errors in $htmMask
    Thinking doing alert without selectively show column by name

--- js/functions.js
+++ js/functions.js
@@ -28,7 +28,7 @@ function cleanBracketString(string){
 	var stringBuilder=string.toString().trim();	stringBuilder=stringBuilder.replace(/[()]/g, "");
  	return stringBuilder.toString().trim();
 }
-function log(str1,str2=null){ 
+function log(str1,str2){ 
 	if(str2){
 		console.log(str1 + " : " + str2);
 	}else{
@@ -43,15 +43,15 @@ assume that always parsing complete sql statement(s)
 Usage: sqlParser.parse(sqlstring) -> get properties: sqlParser.sqlCommand (others like: tableName,hColumns,rColumns)*/
 function sqlParser(){ 
 	this.insertPtn = /^INSERT\s{1,}INTO\s.*[)]\s*VALUES\s*[(].*$/i;
-  this.commandInsertHeadPtn=/^INSERT\sINTO\s*/i;
-  this.tableNamePtn=/^[^,]+[(]/g;
-  this.commandMidValuesPtn = /[)]\s*VALUES\s*[(]/gi;
-  this.commandEndPtn = /[)][;]\s*$/g;
+  	this.commandInsertHeadPtn=/^INSERT\sINTO\s*/i;
+	this.tableNamePtn=/^[^,]+[(]/g;
+	this.commandMidValuesPtn = /[)]\s*VALUES\s*[(]/gi;
+	this.commandEndPtn = /[)][;]\s*$/g;
 
-  this.id=0;
-  this.nextId = function(){ return ++this.id }
-  
-  this.parseStr2Json = function(sqlString){
+	this.id=0;
+	this.nextId = function(){ return ++this.id }
+
+	this.parseStr2Json = function(sqlString){
   	if(this.insertPtn.test(sqlString)){
   		var id = this.nextId();
     	var com,tname,hCols,rCols,hArr,rArr,json;
@@ -86,56 +86,104 @@ function sqlParser(){
   }
 }// ***endOf sqlParser
 
+function arrayHasItem(arr,item){
+	var it = item.toLowerCase();
+	for(var i=0, len=arr.length; i<len; i++){
+		if(arr[i].toLowerCase() == it) return true;
+	}
+	return false;
+}
 
 function sqlRecorder(){
-	this.records=[];
-	this.init = function(){ this.records=[]; }
-//	this.add = function(json){ // json shoud be from sqlParser.getJson()
-//		if(json){this.records.push(json);}
-//	}
+	this.records=[]; //{"id":id,"tname":tname,"command":com,"hArray":hArr,"rArray":rArr}
+	// 
+
+	this.temp=[];
+
+	this.init = function(){ this.records.length=0; this.temp.length=0;}
+
+	this.searchTName = function(tname){
+		// get search and store in temp array
+		if(tname.length>0){
+			var tname = tname.toLowerCase();
+			this.temp.length=0;
+			for(var i=0,len=this.getSize(); i<len; i++){
+				var item = this.records[i];
+				if(item.tname.toLowerCase() == tname) { this.temp.push(item); }
+			}
+			return this.getAllHtms(this.temp);
+		}else{
+			return this.getAllHtms();
+		}
+	}
+
+	this.searchCName = function(cname){
+		if(cname.length>0){
+			var cname = cname.toLowerCase();
+			this.temp.length=0;
+			for(var i=0,len=this.getSize(); i<len; i++){
+				var item = this.records[i];
+				var cnames = item.hArray;
+				if(arrayHasItem(cnames,cname)) { this.temp.push(item); }
+			}
+			return this.getAllHtms(this.temp);
+		}else{
+			return this.getAllHtms();
+		}
+	}
+
 	this.addJson2List = function(json){
 		if(json){this.records.push(json);return 1; }else{return null};
 	}
-	//this.get = function(id=null){
-	//	if(!id){   return this.records;   } else {   var index = this.getIndexById(id);if(index){return this.records[index];}else{return null;}   }
-//	}
-
 
 	this.getIndexById = function(id){
 		if(this.getSize() <=0 ) return null;
 
 		for(var i=0, len=this.getSize();i<len;i++){  if(this.records[i].id==id){return i;}  };return null;
 	}
-//	this.getLastJson = function(){ return this.records[this.records.length-1]; }
+	//	this.getLastJson = function(){ return this.records[this.records.length-1]; }
 	this.getLastHtms = function(){
 		if(this.getSize() <=0 ) return null;
 		return this.getHtmsByIndex(this.records.length-1);
 	}
-	this.getHtmsByIndex = function(i){
-		var size = this.getSize();
-		if(size <=0 || size <= i || isNaN(i) || i < 0) { log("wrong index");return null;}
+	this.getHtmsByIndex = function(i,arr=null){
+		var array;
+		if(Array.isArray(arr)){ 
+			if(arr.length <= 0){return null; } else { array=arr;  }
+			
+		} 
+		else { array=this.records; }
 
-		var rc=this.getItem(i);
+		var len = array.length
+		if(len <=0 || len <= i || isNaN(i) || i < 0) { log("wrong index");return null;}
+
+		var rc=array[i];
 		var idx = rc.id;
-  	var com = rc.command;
-  	var tname = rc.tname;
-  	var rhtm = arr2cells(rc.rArray,'r',idx);
-  	var hhtm = arr2cells(rc.hArray,'h',idx);
-  	htmr="<tr data-id="+idx+" class='responsable'><td data-id="+idx+" data-cid='-2' class='tbcomm'>"+com+"</td><td data-id="+idx+" data-cid='-1' class='tbname hidden'>"+tname+"</td>"+rhtm+"</tr>";
-  	htmh="<tr data-id="+idx+" class='responsable'><th data-id="+idx+" data-cid='-2' class='hidden'>"+com+"</th><th data-id="+idx+" data-cid='-1' class='tbname'>"+tname+"</th>"+hhtm+"</tr>";
+	  	var com = rc.command;
+	  	var tname = rc.tname;
+	  	var rhtm = arr2cells(rc.rArray,'r',idx);
+	  	var hhtm = arr2cells(rc.hArray,'h',idx);
+	  	//htmr="<tr data-id="+idx+" class='responsable'><td data-id="+idx+" data-cid='-2' class='tbcomm'>"+com+"</td><td data-id="+idx+" data-cid='-1' class='tbname hidden'>"+tname+"</td>"+rhtm+"</tr>";
+	  	//htmh="<tr data-id="+idx+" class='responsable'><th data-id="+idx+" data-cid='-2' class='hidden'>"+com+"</th><th data-id="+idx+" data-cid='-1' class='tbname'>"+tname+"</th>"+hhtm+"</tr>";
+		htmr="<tr data-id="+idx+" class='responsable'><td data-id="+idx+" data-cid='-2' class='tbcomm headCell'>"+com+"</td><td data-id="+idx+" data-cid='-1' class='tbname alwaysHidden'>"+tname+"</td>"+rhtm+"</tr>";
+	  	htmh="<tr data-id="+idx+" class='responsable'><th data-id="+idx+" data-cid='-2' class='alwaysHidden'>"+com+"</th><th data-id="+idx+" data-cid='-1' class='tbname headCell'>"+tname+"</th>"+hhtm+"</tr>";
+		
 		return [htmh,htmr];
   }
-  this.getAllHtms = function(){
-  	var result = [];
-  	for(var i=0,len=this.getSize(); i<len; i++){
+  this.getAllHtms = function(arr=null){
+  	var array, result = [];
+  	if(arr) { array = arr } else { array = this.records;}
+  	
+  	for(var i=0,len=array.length; i<len; i++){
   		result.push(this.getHtmsByIndex(i)); 
   	}
   	return result;
   }
+  /*
   this.getItem = function(i){
   	//if(i>= this.getSize())
   	return this.records[i];
-  }
+  }*/
   this.getSize = function(){
   	return this.records.length;
   }
@@ -210,7 +258,6 @@ function cellTracker(){
 			this.reset();
 
 		}else{
-
 			this.reset();
 			this.d_id=d_id;
 			this.d_cid=d_cid;
@@ -240,3 +287,280 @@ function getTrByAtt(attrName='data-id',attrValue){
 	for(var i=0,len=rows.length;i<len;i++){ if(rows[i].getAttribute(attrName)==attrValue) return rows[i] }
 	return null;
 }
+
+
+function arraylise(string){
+    var array=[];
+    var str = string.toString();
+    for(var i=0,len=str.length; i<len; i++){
+        array.push(str.substring(i,i+1));
+    }
+    return array;
+}
+/*
+function getMaxLen(array){
+    var max=0;
+    for(var i=0,len=array.length; i<len; i++){
+        var l=array[i].toString().length;
+        if(max < l) max=l;
+    }
+    return max;
+}
+
+function getMax(array){
+    var max=0;
+    for(var i=0,len=array.length; i<len; i++){
+        if(max<array[i]) max=array[i];
+    }
+    return max;
+}
+*/
+/**Compare targets from $htmMask.showCol() -> getTargetElements() */
+function textCompare(){
+	this.threshold1 = 0; // level 1 -> compareAlert1 (pink, might be error)
+	this.threshold2 = -10; // level 2 -> compareAlert2 (red, should be error)
+	this.alertClass1 = 'compareAlert1';
+	this.alertClass2 = 'compareAlert2';
+	this.elements=[];
+	this.scores = [];
+	this.popularTypes = [];
+	this.alertCount = 0;
+    this.getCharType = function(c){
+    	if(c.toString().length==0){ /*log("empty");*/ return -1; }
+	    if(/\d/.test(c)) { return 1; }
+	    if(/\w/.test(c)) { return 2; }
+	    if(/\s/.test(c)) { return 3; }
+	    //else if(/^$/.test(c)) { return 9;}
+	    else { return 4; }
+    }
+    this.getPopularType = function(types){
+        var ones=0,twos=0,threes=0,fours=0, nones=0;
+        for(var i=0, len=types.length; i<len; i++){
+            switch(types[i].toString()){
+                case "1" : ones++; break;
+                case "2" : twos++; break;
+                case "3" : threes++; break;
+                case "4" : fours++; break;
+                case "-1" : nones++; break;
+                default: break;
+            }
+        }
+        // ones   twos   threes   fours
+        // 1        3     3          2
+        var maxType, max;
+        max=ones; maxType=1;
+        if(max < twos) { max=twos; maxType=2; }
+        //else if(max==twos){}
+        if(max < threes) { max=threes; maxType=3; }
+    	if(max < fours) { max=fours; maxType=4; }
+    	if(max < nones) { max=nones; maxType=-1; }
+    	//log("now pop from types=",maxType)
+        return maxType; // get most popular type, like 2
+    }
+    this.getMaxColLength = function(array){
+		var max=0;
+		for(var i=0,len=array.length; i<len; i++){
+			var size = array[i].length;
+			if(max < size){ max=size; }
+		}
+		return max;
+	}
+	
+    this.compare = function(elements){
+    	this.alertCount =0;
+    	document.getElementById("alertCount").innerHTML = this.alertCount;
+    	
+    	if(!elements || elements.length <=0){ return null;}
+    	this.elements.length = 0;
+    	this.elements = elements;
+        var txtArray=[], result = [];
+        var charArray = [];
+        var typeArray = [];
+
+        for(var i=0,len=this.elements.length; i<len; i++){
+        	var txt = this.elements[i].text();
+        	if(!txt || txt == "null"){ charArray.push(""); }
+        	else{ charArray.push(arraylise(txt)); }
+	    }
+	    /* now we have array like: charArray = 
+	    	[['s','3',',',' ','n','g'],
+	    	 ['s','3',',',' ','n','g'],
+	    	 ['s','3','',' ','n','g']]
+	    */
+	    for(var y=0, rowCount=charArray.length; y<rowCount; y++){
+	    	var types=[];
+	    	for(var x=0, colCount=charArray[y].length; x<colCount; x++){
+	    		types.push(this.getCharType(charArray[y][x]));
+	    	}
+	    	typeArray.push(types);
+	    }
+	   	/* now we have array like: typeArray =
+	        [[ 2 , 1 , 4 , 3 , 2 , 2 ],
+	         [ 2 , 1 , 4 , 3 , 2 , 2 ],
+	         [ 2 , 1 , -1, 3 , 2 , 2 ]]
+	    */
+	    // need to nomarlize.   empty => -1
+	    for(var x=0, maxColCount=this.getMaxColLength(typeArray); x<maxColCount; x++){
+	    	var types=[];
+	    	for(var y=0, rowCount=typeArray.length; y<rowCount; y++){
+	    		if(!typeArray[y][x]){ typeArray[y][x] = "-1"; /*log('empty=',typeArray[y][x])*/}
+	    	}	
+	    }
+/*
+	    log("type.rowIdx7",typeArray[7])
+	    log("type.rowIdx8=null",typeArray[8])
+	    log("type.rowIdx9=845448536655145739",typeArray[9])
+	    log("type.rowIdx10=null",typeArray[10])
+	    log("type.rowIdx11=null",typeArray[11])
+	    log("type.rowIdx12=null",typeArray[12])
+	    log("type.rowIdx13=null",typeArray[13])
+	    log("type.rowIdx14",typeArray[14])
+*/
+	    this.popularTypes.length = 0;
+	    for(var x=0, maxColCount=this.getMaxColLength(typeArray); x<maxColCount; x++){
+	    	var types=[];
+	    	for(var y=0, rowCount=typeArray.length; y<rowCount; y++){
+	    		types.push(typeArray[y][x]);
+	    	}
+	    	this.popularTypes.push(this.getPopularType(types));
+	    }
+	    log("popular types",this.popularTypes);  // *****************************
+
+	    // get a score for each row/td-cell-text, and push it into this.scores
+	    this.scores.length =0;
+	    for(var y=0,len=typeArray.length; y<len; y++){
+	    	this.getScoresPerRowAndPush(typeArray[y]);
+			/*if(score <= this.threshold2){ 
+				var e = this.elements[y];
+				e.removeClass(this.alertClass1);
+				e.removeClass(this.alertClass2);
+				this.elements[y].addClass(this.alertClass2)
+			}
+			else if(score <= this.threshold1){ 
+				var e = this.elements[y];
+				e.removeClass(this.alertClass1);
+				e.removeClass(this.alertClass2);
+				this.elements[y].addClass(this.alertClass1)
+			}*/
+	    }
+	   		   	
+	   	log("scores",this.scores);// *****************************
+	    // get MMScore, which is (mean + median)/2, and use it to determain the distance between row/cell's text & MMScore
+	   	var mmScore=this.getMMScore(this.scores);// *****************************
+	   	log("mmScore",mmScore);
+	   	// get MMDistance, which is used to trigger alert when distance greater than MMDistance
+	   	var mmDistance = this.getMMDistance(this.scores,mmScore);
+
+	   	console.log("mmDistance="+mmDistance)
+	   	for(var i=0,len=this.scores.length; i<len; i++){
+	   		var e = this.elements[i];
+	   		var alertLevel = this.getDistanceLevel(this.scores[i],mmScore,mmDistance);
+
+	   		switch(alertLevel){
+	   			case 2:
+		   			e.removeClass(this.alertClass1);
+					e.removeClass(this.alertClass2);
+					this.elements[i].addClass(this.alertClass2);
+					this.alertCount++;
+				break;
+				case 1:
+					var e = this.elements[i];
+					e.removeClass(this.alertClass1);
+					e.removeClass(this.alertClass2);
+					this.elements[i].addClass(this.alertClass1);
+					this.alertCount++;
+				break;
+				default:
+				break;
+	   		}
+    	document.getElementById("alertCount").innerHTML = this.alertCount;
+
+	   	}
+	   	// getDistLevel(mmDistance)  and show alert
+/*
+	   	for(var i=0,len=this.scores.length; i<len; i++){
+	    	totalDist += this.getDistanceLevelxxx(this.scores[i],mmScore);
+
+
+			if(score <= this.threshold2){ 
+				var e = this.elements[i];
+				e.removeClass(this.alertClass1);
+				e.removeClass(this.alertClass2);
+				this.elements[i].addClass(this.alertClass2)
+			}
+			else if(score <= this.threshold1){ 
+				var e = this.elements[i];
+				e.removeClass(this.alertClass1);
+				e.removeClass(this.alertClass2);
+				this.elements[i].addClass(this.alertClass1)
+			}
+	    }*/
+    }
+    this.getMMDistance = function(scores,mmScore){
+
+    	var totalDist =0;
+    	var len=scores.length;
+    	var distArray=[]
+    
+	   	for(var i=0; i<len; i++){
+	   		var dis = +(scores[i] - mmScore);
+	   		if(dis<0) dis=-dis;
+			//console.log("dis = "+dis);
+	   		distArray.push(dis);
+	    	totalDist += dis;
+	    }
+	    //console.log('totalDist = ' + totalDist)
+		var meanDist = totalDist/len;
+		//console.log("meanDist = " + meanDist)
+		distArray.sort();
+		var medianDist = distArray[Math.floor(distArray.length / 2)];
+		//console.log("median = "+medianDist)
+		var mmDis = (+meanDist + medianDist)/2;
+		//console.log("in getmMd, mmDis=" + mmDis)
+		return mmDis;
+
+    }
+    /**hight distanceLevel raises alert for user to see if there are errors*/
+    this.getDistanceLevel = function(score,mmScore,mmDistance){
+    	var distance = Math.abs(+score - mmScore);
+    	var level=0;
+    	if(mmDistance!=0){
+    		if(distance >= mmDistance*1.5){
+	    		level = 2;
+	    	}else if(distance > mmDistance){
+	    		level = 1;
+	    	}
+    	}
+
+    	console.log(">>distance(score="+score+") : "+distance + " => level "+ level);
+    	return level;
+    }
+    // scores : 28,28,28,28,28,28,28,28,-4,-4,-4,-4,-4,-4,28,28,28,28,28,28,28,28,28,28,28,28,28,28,28,28,28,28,28,28,28,28,28,28,28,28
+    // total  : 928
+    // mean	  : 23.2
+    // median : 
+    this.getMMScore = function(scores){
+    	var total=0;
+    	var len=scores.length;
+    	for(var i=0; i<len; i++){ total+=scores[i]; }
+    	var mean = total/len;
+    	var sarray = scores.slice(0).sort(); // make a clone of values only, without referencing; then sort it
+    	var median = sarray[Math.floor(sarray.length / 2)];
+    	return (mean+median)/2;
+    }
+    
+	this.getScoresPerRowAndPush = function(targetArray){
+		// compare -> score -> store in this.scores, on number value per row
+		/*  popular types: 	4,2,2,2,2,2,2,2, 2, 2, 2, 4
+			row's types: 	4,2,2,2,2,2,2,4,-1,-1,-1,-1
+		*/
+		var s=0;
+		for(var x=0,len=this.popularTypes.length; x<len; x++){
+			if(this.popularTypes[x] == targetArray[x]){ s++; } else { s--; }
+		}
+
+		this.scores.push(s);
+		return s;
+	}// endof this.getScoresPerRowAndPush
+
+}// endof textCompare
--- js/main.js
+++ js/main.js
@@ -7,8 +7,6 @@ jQuery( document ).ready(function( $ ) {
 
 // jq functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   function $htmWriter(){
-    //this.lastRow = $('tr.hiddenEnd');
-    //this.lastRowHtm = '<tr class="hiddenEnd"></tr>';
 
     this.table = $('.resultTable');
     this.init = function(){
@@ -28,7 +26,6 @@ jQuery( document ).ready(function( $ ) {
       }
     }
     this.writeAll = function(htmsArr){
-      log("writeAll="+htmsArr.length)
       if(Array.isArray(htmsArr)){
         for(var i=0,len=htmsArr.length; i<len; i++){
           this.writeOne(htmsArr[i],true);
@@ -39,14 +36,82 @@ jQuery( document ).ready(function( $ ) {
 function $refreshCount(){
     $('#rowCount').text(recorder.getSize());
 }
-// 1
-  var parser = new sqlParser();
-  var recorder = new sqlRecorder();
-  var tracker = new cellTracker();
-  var lines;
-  var $writer = new $htmWriter();
+
+
+function $htmMask(){
+  this.table = $('.resultTable');
+  this.hc1 = 'mhide';
+  this.hc2 = 'hidden';
+  this.ptns = [
+      {"num": /^\d+$/},
+  ];
+  this.targetElements = [];
+  this.cleanUp = function(){
+    $("th").removeClass(this.hc1);
+    $("td").removeClass(this.hc1);
+    $("th").removeClass(this.hc2);
+    $("td").removeClass(this.hc2);
+    
+  }
+  this.applyAll = function(myclass){
+
+    $("th").addClass(myclass);
+    $("td").addClass(myclass);
+  }
+/*
+  this.guessPtn = function(elements){
+    var yess=0, nos=0;
+    var aarr = [];
+    var glass = [];
+    for(var i=0,len=elements.length; i<len; i++){
+        //log(array[i])
+        aarr.push(arraylise(elements[i].text()));
+    }
+    var ct = new cTypeComp();
+    for(var ci=0, clen=getMaxLen(elements); ci<len; ci++){
+        for(var ri=0, rlen=elements.length; ri<rlen; ri++){
+            ct.add(cType(aarr[ri][ci]))
+            //log("ctype",cType(aarr[ri][ci]));
+        }
+        log(ct.compare());
+        ct.init();
+
+    }
+  }*/
+    this.showCol = function(cname){
+        this.cleanUp();
+        var hc;
+        var tgtElements=[];
+        //this.targetElements=[];
+
+        if($('input[name=exclusion]:checked').val()=="grayout"){ hc=this.hc1; }else{ hc=this.hc2; }
+
+        if(!cname){ return; }
+        this.applyAll(hc);
+
+        var ths=$("th:containsExact("+cname.toUpperCase()+")").not('.headCell');
+        //log("ths num",ths.length)
+        $('span#rowCount2').text(ths.length);
+        if(ths.length <=0){ return;}
+        ths.removeClass(hc);
+        ths.each(function(){
+          var cid=$(this).attr("data-cid");
+          //$(this).parent('tr').next('tr').children("td[data-cid="+cid+"]").removeClass(hc);
+          var tg = $(this).parent('tr').next('tr').children("td[data-cid="+cid+"]");
+          tg.removeClass(hc);
+          tgtElements.push(tg);
+        });
+        this.targetElements.length=0;
+        this.targetElements = tgtElements;
+
+    }
+    this.getTargetElements = function(){
+    return this.targetElements;
+    };
+  
+}
 // 2 ?
-function readSingleFile(evt) {
+function $readSingleFile(evt) {
     //Retrieve the first (and only!) File from the FileList object
     var f = evt.target.files[0];     
     if (f) {
@@ -55,49 +120,57 @@ function readSingleFile(evt) {
       var loaded=[];
       var ignores=[];
       r.onload = function(e) {   
-	      var content = e.target.result;   //f.name,f.type,f.size
+          var content = e.target.result;   //f.name,f.type,f.size
 
         lines = content.split(/[\r\n]+/g); // tolerate both Windows and Unix linebreaks
-	      if(lines.length >0){
-	      	for(var i=0,len=lines.length;i<len;i++){
-	      		var feedback = recorder.addJson2List(parser.parseStr2Json(lines[i]));
-	      		if(feedback){
-      				$writer.writeOne(recorder.getLastHtms());
-				      loaded.push(i+1);
-	      		}else{
-	      			ignores.push(i+1);
-	      		}
-	      	}
-	      	if(ignores.length > 0){
-	      		log("Loaded "+loaded.length + "/"+ lines.length + " : "+ loaded+"\n"+
-	      			"Ignored "+ignores.length + "/"+ lines.length + " : "+ ignores)
-	      	}
+          if(lines.length >0){
+            for(var i=0,len=lines.length;i<len;i++){
+                var feedback = recorder.addJson2List(parser.parseStr2Json(lines[i]));
+                if(feedback){
+                    $writer.writeOne(recorder.getLastHtms());
+                      loaded.push(i+1);
+                }else{
+                    ignores.push(i+1);
+                }
+            }
+            if(ignores.length > 0){
+                log("Loaded "+loaded.length + "/"+ lines.length + " : "+ loaded+"\n"+
+                    "Ignored "+ignores.length + "/"+ lines.length + " : "+ ignores)
+            }
           $refreshCount();
-	      }
+          }
       }
       r.readAsText(f);
 
       log('File loaded');
-    } else {     	
+    } else {        
       alert("Failed to load file");
       return null;
     }
   }
 
-
+// -------------------------------- jq main -------------------
+// 1
+  var parser = new sqlParser();
+  var recorder = new sqlRecorder();
+  var tracker = new cellTracker();
+  var lines;
+  var $writer = new $htmWriter();
+  var $mask = new $htmMask();
+  var compare = new textCompare();
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 3. Events ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	
+    
 
   window.onscroll = function() {
-  	$('#scroll').text("screen.height="+document.body.scrollLeft+", documentElement.scrollLeft="+document.documentElement.scrollLeft);
+    $('#scroll').text("screen.height="+document.body.scrollLeft+", documentElement.scrollLeft="+document.documentElement.scrollLeft);
   };
 
-	if (window.File && window.FileReader && window.FileList && window.Blob) {
-		  document.getElementById('fileinput').addEventListener('change', readSingleFile, false);
-		} else {
-			$("#fileinput").prop('disabled', true);
-		  alert('The File APIs are not fully supported by your browser.');
-		}
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+          document.getElementById('fileinput').addEventListener('change', $readSingleFile, false);
+        } else {
+            $("#fileinput").prop('disabled', true);
+          alert('The File APIs are not fully supported by your browser.');
+        }
 
   //.containsExact("sometext") use like .text("sometext")
   $.expr[":"].containsExact = function (obj, index, meta, stack) {
@@ -115,7 +188,41 @@ function readSingleFile(evt) {
       }
 
   });// ***endOf btnConvert.click
-  
+  $('#txtSchId').on('keydown', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { // 13 is enter
+
+      var index = $(this).val();
+      $writer.init();
+      if(!index){
+        $writer.writeAll(recorder.getAllHtms());
+      }else{
+        $writer.writeOne(recorder.getHtmsByIndex(index));
+      }
+      $refreshCount();
+    }
+  });
+
+  $('#txtSchTName').on('keydown', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { // 13 is enter
+      var value = $(this).val();
+      $writer.init();
+      $writer.writeAll(recorder.searchTName(value));
+      $refreshCount();
+    }
+  });
+
+  $('#txtSchCName').on('keydown', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { // 13 is enter
+      var value = $(this).val();
+      $writer.init();
+      $writer.writeAll(recorder.searchCName(value));
+      $refreshCount();
+    }
+  });
+
   //btnSch txtSchId txtSchTName txtSchCName txtSchCValue
   $('#btnSch').click(function(){
     var index = $('#txtSchId').val();
@@ -128,7 +235,34 @@ function readSingleFile(evt) {
     $refreshCount();
 
   });
+// selector 2
+  $('#txtShowCol').on('keydown', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13 || key == "ENTER") { // 13 is enter
+      $mask.showCol($(this).val());
+      if($(this).val().length >0){
+        compare.compare($mask.getTargetElements());
+      }
+    }
+  });
 
+  //$('input:radio#exc_grayout').change(function(){
+  $('input:radio[name=exclusion]').change(function(){
+
+    $('#txtShowCol').trigger({type:'keydown',which:13})   // or which:"ENTER"    13
+    /*
+    var r=$(this)
+    if(r.is(":checked") && r.val() == "grayout"){
+      log("grayout")
+      //{ type : 'keypress', which : character.charCodeAt(0) }
+      $('#txtShowCol').trigger({type:'keydown',which:'ENTER'})
+
+    }
+    if(r.is(":checked") && r.val() == "hide"){
+      log("hide")
+      $('#txtShowCol').trigger({type:'keydown',which:'ENTER'})
+    }*/
+  })
 
   $('.refresh').click(function(){
     $('#rowCount').text(recorder.getSize());
@@ -143,7 +277,7 @@ function readSingleFile(evt) {
                   var d_cid = $(this).attr('data-cid');
 
                   tracker.trackCell(d_id, d_cid)
-                log("cell@ "+tracker.x+","+tracker.y+","+tracker.w+","+tracker.h);
+                //log("cell@ "+tracker.x+","+tracker.y+","+tracker.w+","+tracker.h);
   });
 
               
